aee502ae23529297eaa98f0c4b010f45e3e8c2cb
Lawani-EJ <elyonjohn60@gmail.com>
2024-07-25 22:55:51 +0100

    Removed bootstrap link

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
