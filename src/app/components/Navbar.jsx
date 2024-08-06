4564c9bf54df0e4dd228e1e36aafb81c9ece638e
Lawani-EJ <elyonjohn60@gmail.com>
2024-08-06 21:17:43 +0100

    Modified the navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -5,10 +5,10 @@ const Navbar = () => {
   return (
 <nav className="bg-white border-gray-200 dark:bg-gray-900">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+    <Link href={`/`} class="flex items-center space-x-3 rtl:space-x-reverse">
         <img src="https://e7.pngegg.com/pngimages/151/227/png-clipart-black-logo-illustration-minimalism-tattoo-geometry-logo-free-buckle-triangle-element-angle-free-logo-design-template-thumbnail.png" class="h-8" alt="EnjoyLiving Logo" />
         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">EnjoyLiving</span>
-    </a>
+    </Link>
     <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
